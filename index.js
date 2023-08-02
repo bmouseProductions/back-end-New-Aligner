6e3e248d4f1ec0ab443de11f17e957c254930e5e
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-08-02 18:20:19 -0300

    teste OC

--- index.js
+++ index.js
@@ -2,55 +2,66 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require("cors")
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-async function enviarEmailBackend(nome, email, telefone) {
+async function enviarEmailBackend(
+  nome,
+  email,
+  telefone,
+) {
   try {
     let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: 'smtp-mail.outlook.com',
       port: 587,
       secure: false,
       auth: {
-        user: 'mktnewaligner@gmail.com', // Use variável de ambiente para o usuário do e-mail
-        pass: 'Contasmouse23@' // Use variável de ambiente para a senha do e-mail
+        user: 'site@patense.com.br',
+        pass: 'p!t@n#e0623',
       },
     });
 
     let info = await transporter.sendMail({
-      from: 'mktnewaligner@gmail.co', // Use o mesmo usuário do e-mail como remetente
-      to: "matheustxr.profissional@gmail.com",
-      subject: "Quero ser um credenciado New Aligner",
+      from: 'site@patense.com.br',
+      to: ["matheustxr.profissional@gmail.com"], 
+      subject: 'Gostaria de saber mais informações sobre as farinhas de Camarão e Atum',
       html: `<p>Nome: ${nome}</p>
-               <p>Telefone: ${telefone}</p>
-               <p>E-mail: ${email}</p>` 
-    })
-    console.log("E-mail enviado: %s", info.messageId);
-  } catch(err) {
-    console.error('Erro ao enviar o e-mail', err);
-    throw err;
+             <p>E-mail: ${email}</p>
+             <p>Telefone: ${telefone}</p>`,
+    });
+    console.log('email send: %s', info.messageId);
+  } catch (err) {
+    console.error(err);
   }
 }
 
 app.post('/send', async (req, res) => {
-  const { nome, email, telefone } = req.body;
+  const { 
+    nome, 
+    email, 
+    telefone,
+  } = req.body; // Receba todos os campos do corpo da requisição
 
   try {
-    // Realizar a validação dos dados aqui, se necessário
+    await enviarEmailBackend(
+      nome,
+      email,
+      telefone,
+    );
 
-    await enviarEmailBackend(nome, email, telefone);
-
-    res.setHeader('Access-Control-Allow-Origin', 'https://new-aligner.netlify.app/'); // Defina o domínio do seu site aqui
-    res.status(200).json({ msg: 'E-mail enviado com sucesso' });
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({ msg: 'Email send success' });
   } catch (error) {
-    console.error('Erro ao enviar o e-mail', error);
-    res.setHeader('Access-Control-Allow-Origin', 'https://new-aligner.netlify.app/'); // Defina o domínio do seu site aqui
-    res.status(500).json({ error: 'Erro ao enviar o e-mail' });
+    console.error('Error sending the email', error);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(500).json({ error: 'Error sending the email' });
   }
 });
 
