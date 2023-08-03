0ca3e17f624a3ff8e7123eac97c1d8a3ea4fcd10
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-08-03 11:15:47 -0300

    teste novo form

--- index.js
+++ index.js
@@ -19,18 +19,13 @@ async function enviarEmailBackend(
 ) {
   try {
     let transporter = nodemailer.createTransport({
-      /*host: 'smtp.gmail.com',
+      host: 'smtp.gmail.com',
       port: 587,
       secure: false,
       auth: {
         user: 'mktnewaligner@gmail.com',
         pass: 'Contasmouse23@',
-      },*/
-      service: 'gmail',
-      auth: {
-        user: 'mktnewaligner@gmail.com',
-        pass: 'Contasmouse23@'
-      }
+      },
     });
 
     let info = await transporter.sendMail({
